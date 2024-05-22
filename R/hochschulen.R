4d76df02363f920dfc03bfaa22a7a41facd1fdb6
Richard Meyer-Eppler <richard.meyereppler@gmail.com>
2024-05-22 21:50:00 +0200

    Initial commit

---
+++ R/hochschulen.R
@@ -0,0 +1,11 @@
+#' Hochschulen - Higher education institutions in Germany
+#'
+#' Tbc
+#'
+#' @format A tibble with `r get_nrow(hochschulen)` rows and `r get_ncol(hochschulen)` variables:
+#' \describe{
+#'  \item{Hochschule_Signatur}{3-digit Destatis ID}
+#' }
+#'
+#' @examples
+#' hochschulen
